1f47a32335c14f65c8f855176fae18b838e9ee02
Kevin Charles <k@surbitcoin.com>
2016-08-31 13:48:56 -0400

    Feedbacks from Teamthreehouse

--- js/content.js
+++ js/content.js
@@ -1,3 +1,4 @@
+// Array of objects tha contain different quotes that will be diplayed  to the screen
 var quotes ; 
 quotes = [
 	{
@@ -5,35 +6,36 @@ quotes = [
 		source:"Bill Gates",
 		year:"1998",
 		citation:"Microsoft Conference",
-		tag:"Technology"
+		//tag:"Technology"
 	},
 	{
 		quote:"I believe life is an intelligent thing: that things aren't random.",
 		source:"Steve Jobs",
 		year:"2002",
 		citation:"Auto-Biography",
-		tag:"Technology"
+		//tag:"Technology"
 	},
 	{
 		quote:"It takes 20 years to build a reputation and five minutes to ruin it. If you think about that, you'll do things differently.",
 		source:"Warren Buffet",
 		year:"2005",
 		citation:"Berkshire Hattaway",
-		tag:"business"
+		//tag:"business"
 	},
 	{
 		quote:"Most people spend more time and energy going around problems than in trying to solve them.",
 		source:"Henry Ford",
-		tag:"business"
+		//tag:"business"
 		
 	},
 	{
 		quote:"When one door is closed, don't you know, another is open.",
 		source:"Bob Marley",
 		citation:"Album",
-		tag:"Music"
+		//tag:"Music"
 	}
 	];
 
+// Empy array that will contain the viewed quotes
 var viwedQuotes ;
 	viwedQuotes = [];
--- js/quotes_generator.js
+++ js/quotes_generator.js
@@ -1,8 +1,8 @@
 
-
+// Get a random quote from the Array by creating a random number that will call it.
 function getRandomQuote(){
 
-
+	// once all the quotes has been viewed restart the original array.
 	if (quotes.length === 0) {
 	quotes = viwedQuotes;
 	viwedQuotes = [];
@@ -11,55 +11,53 @@ function getRandomQuote(){
 	var number = Math.floor(Math.random()*(quotes.length));	
 
 	var randomQuote = quotes[number];
+	//push the viewed quote into de new array
 	viwedQuotes.push(randomQuote);
+	//splice the viewed quote from the array so it cant be repeted
 	quotes.splice(number, 1);
 	return randomQuote;
 		
 }
 
-
+// Print the randomly generated quote to the screen
 function printQuote(){
 
 	var toDisplay = getRandomQuote();
 
 
+
+	if ( toDisplay.quote && toDisplay.source ) {
 	var quote =  '<p class="quote"> ' + toDisplay.quote + ' </p> ' + '<p class="source">' +  toDisplay.source ;
-	var citation  = '<span class="citation">' +  toDisplay.citation  ;	
-	var year = '<span class="year">' +  toDisplay.year + '</span> </p>' ;
-	var message ; 
 
-	if ( toDisplay.source && toDisplay.citation && toDisplay.year) {
+    } else if (toDisplay.citation && toDisplay.year) {
+
+    quote  += '<span class="citation">' +  toDisplay.citation  + '<span class="year">' +  toDisplay.year +'</span></p>' ;
+
+
+    } else if (toDisplay.year) {
+
+    quote += '<span class="year">' +  toDisplay.year  + '</span> </p>';
 	
-	message = quote + citation + year ;
+	} else if (toDisplay.citation) {
 
-    } else if (toDisplay.source && toDisplay.citation) {
+	quote += '<span class="citation">' +  toDisplay.citation + '</span> </p>';
 
-    message = quote + citation + '</span></p>';
-
-    } else if ( toDisplay.source  && toDisplay.year) {
-
-    message =  quote + year ;
-
-    } else if (toDisplay.source) {
-
-    message = quote + '</p>';
-
-    }
+	}
 	
 	
-	toShow(message);
-	return message;
+	toShow(quote);
+	return quote;
 	
 	
 }
 
-function toShow(message) {
+function toShow(quote) {
 
-   
+   		
 	
         var changeMessage = document.getElementById('quote-box');
-		changeMessage.innerHTML = message;
-	
+		changeMessage.innerHTML = quote;
+		window.clearInterval(intervalID);
 }
 
 
